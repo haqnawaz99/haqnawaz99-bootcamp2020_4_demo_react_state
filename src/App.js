a1fcad0727ade04cd58360c2681a50e232c3a262
Haq Nawaz <haqnawaz99@yahoo.com>
2020-08-25 18:17:01 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './index.css'; 
+import './App.css';
+import Room from './Room';
+
+function App() {
+  return (
+    <div>
+      <Room/>
+      <Room/>
+      <Room/>
+      <Room/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Room.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import './App.css';
+import './Room.css';
+function Room() {
+    let [isLit, setLit] = useState(false);
+    let [age, setAge] = useState(23);
+    function updatelit(){
+        setLit(!isLit);
+    }
+  return (
+    <div className={"room " + (isLit?"lit":"dark") }>
+       Lit = {isLit? "lit" : "dark"}<br/>
+       age = {age}
+      <button onClick={() => setLit(!isLit)}>
+          Toggle Light</button>
+      <button onClick={ () => {
+                    setAge(++age)
+      }}>Increase Age</button>
+      <br/>
+      
+    </div>
+  );
+}
+
+export default Room;
